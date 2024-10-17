d89abb8a72a37fb26572fa76c5f8b2c5d97a7351
Ice Roongpetch <roongpetch@beliveasia.com>
2024-10-17 16:15:16 +0700

    fix: re-config mailer

--- startup.relational.dev.sh
+++ startup.relational.dev.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-/opt/wait-for-it.sh postgres:5432
+/opt/wait-for-it.sh db:3306
+/opt/wait-for-it.sh maildev:1025
 npm run migration:run
 npm run seed:run:relational
 npm run start:prod
